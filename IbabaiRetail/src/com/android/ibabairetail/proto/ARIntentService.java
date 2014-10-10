8b28d08001489081afbef78aa383ab3380096c19
Oleg Y <oleg.yakimets@gmail.com>
2014-10-10 17:43:24 +0300

    Add Intent services

---
+++ IbabaiRetail/src/com/android/ibabairetail/proto/ARIntentService.java
@@ -0,0 +1,16 @@
+package com.android.ibabairetail.proto;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ARIntentService extends IntentService {
+	ARIntentService() {
+		 super("ARIntentService");
+	}
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IbabaiRetail/src/com/android/ibabairetail/proto/ReceiveTransitionsIntentService.java
@@ -0,0 +1,17 @@
+package com.android.ibabairetail.proto;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ReceiveTransitionsIntentService extends IntentService {
+	ReceiveTransitionsIntentService() {
+		super("ReceiveTransitionsIntentService");
+	}
+
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		
+	}
+
+}
